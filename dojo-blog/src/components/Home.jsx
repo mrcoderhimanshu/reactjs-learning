38b8ac2f1720f895531703b80740e3f8aff815dc
Himanshu Phoolwar <mr.coder.himanshu@gmail.com>
2021-08-28 14:46:22 +0530

    add: useState example addition

--- dojo-blog/src/components/Home.jsx
+++ dojo-blog/src/components/Home.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 const Home = () => {
+    const [name, setName] = useState('Dusty');
+
     const handleClickButton = () =>{
         console.log('this is how we can create function inside components');
     }
     const handleParamMethod = (abc) => {
         console.log('Param methods are called differently. As we did here. Because if we call directly then it will execute it at very beginig.');
+    }
+    const handleChange = () => {
+        setName('Napster');
     }
     return ( 
         <div>
@@ -11,6 +17,8 @@ const Home = () => {
             <p>Welcome to the Blog.</p>
             <p><button onClick={handleClickButton}>Click Me</button></p>
             <p><button onClick={() =>{handleParamMethod('param')}}>Click Me</button></p>
+            <p>Welcome {name}</p>
+            <p><button onClick={handleChange}>UseState-View</button></p>
         </div>
      );
 }
