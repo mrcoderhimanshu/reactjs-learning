f4d657aaebebfc8f6bad832fd02940d366538bde
Himanshu Phoolwar <mr.coder.himanshu@gmail.com>
2021-08-28 19:53:34 +0530

    add: added a new custom hook called useFetch

--- dojo-blog/src/components/Home.jsx
+++ dojo-blog/src/components/Home.jsx
@@ -1,10 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "../hooks/useFetch";
+
 const Home = () => {
     const [name, setName] = useState('Dusty');
+    const { data: blogs, loading, Error} = useFetch('http://localhost:3000/blogs');
+    /*
     const [blogs, setBlogs] = useState(null);
     const [loading, setLoading] = useState(true);
-    /*
+    const [Error, setError] = useState(null);
+    */
+   /*
     const [blogs, setBlogs] = useState([
         {title:"My new website", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled....", author:"mario", id: 1},
         {title:"Welcome Party!", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled....", author:"dusty", id: 2},
@@ -22,8 +28,8 @@ const Home = () => {
         setName('Napster');
     }
     const handleDelete = (id) =>{
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
+        //const newBlogs = blogs.filter(blog => blog.id !== id);
+        //setData(newBlogs);
     }
     /*
     useEffect(() => {
@@ -33,14 +39,23 @@ const Home = () => {
         //console.log('We can trigger it at a particular state change by adding that state into the array');
     },[name])
     */
+   /*
     useEffect(() =>{
         fetch('http://localhost:3000/blogs').then(res => {
+            if(res.ok !== true){
+                throw Error("Could not find the address.");
+            }
             return res.json();
         }).then(data =>{
             setBlogs(data);
             setLoading(false);
+            setError(null);
+        }).catch(err =>{
+            setError(err);
+            setLoading(false);
         })
-    },[])
+    },[]);
+    */
     return ( 
         <div>
             <h1>Home Page</h1>
@@ -49,12 +64,11 @@ const Home = () => {
             <p><button onClick={() =>{handleParamMethod('param')}}>Click Me</button></p>
             <p>Welcome {name}</p>
             <p><button onClick={handleChange}>UseState-Hook-View</button></p>
+            {Error && <div>{Error}</div>}
             {loading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="Blog List" handleDelete ={handleDelete}/>}
-            
         </div>
-     );
-    
+    );
 }
  
 export default Home;
