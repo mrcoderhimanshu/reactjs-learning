542200b1633cad020c5cf4d025e930e6004a276a
Himanshu Phoolwar <mr.coder.himanshu@gmail.com>
2021-08-28 21:02:43 +0530

    add: link instade of anchor tag

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,14 +1,25 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Contact from "./components/Contact";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
